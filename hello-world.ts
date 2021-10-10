fb999fbab33be6a23eea5f80adaad7e6bdbb8a11
Yasir Tahir <yasirtahir@lewisu.edu>
2021-10-10 02:01:02 -0500

    Made some changesto js and ts.

--- hello-world.ts
+++ hello-world.ts
@@ -1,32 +1,11 @@
+let map: google.maps.Map;
+
 function initMap(): void {
-  const myLatlng = { lat: -25.363, lng: 131.044 };
-
-  const map = new google.maps.Map(
-    document.getElementById("map") as HTMLElement,
-    {
-      zoom: 4,
-      center: myLatlng,
-    }
-  );
-
-  const marker = new google.maps.Marker({
-    position: myLatlng,
-    map,
-    title: "Click to zoom",
+  map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
   });
 
-  map.addListener("center_changed", () => {
-    // 3 seconds after the center of the map has changed, pan back to the
-    // marker.
-    window.setTimeout(() => {
-      map.panTo(marker.getPosition() as google.maps.LatLng);
-    }, 3000);
-  });
-
-  marker.addListener("click", () => {
-    map.setZoom(8);
-    map.setCenter(marker.getPosition() as google.maps.LatLng);
-  });
 
   google.maps.event.addListener( map, "bounds_changed", function() {
     var bounds = map.getBounds();
